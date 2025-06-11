2d3094ba641f4eecb6531d66dadbc225b4eedc19
Tiago Ferreira <tiago_caseiro_95@hotmail.com>
2025-06-12 00:09:12 +0100

    Add UniformBuffer

---
+++ src/opengl/UniformBuffer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include <glad/glad.h>
+
+class UniformBuffer
+{
+public:
+    void init();
+    void uploadUboData(glm::mat4 viewMatrix, glm::mat4 projectionMatrix);
+    void cleanup();
+
+private:
+    GLuint mUboBuffer = 0;
+};
